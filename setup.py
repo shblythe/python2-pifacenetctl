cad34302603e4b1a969df4a755a1e69b59f51f35
Stephen Blythe <stephen@urwick.co.uk>
2014-06-16 21:46:47 +0100

    Added files to build PyPI distribution

---
+++ setup.py
@@ -0,0 +1,15 @@
+# vim: set et sts=4:
+from distutils.core import setup
+
+setup(	name='pifacenetctl',
+    description='Raspberry Pi and PiFace headless network manager',
+    author='Stephen Blythe',
+    author_email='stephen@urwick.co.uk',
+    url='https://github.com/shblythe/python2-pifacenetctl',
+    version='0.1.0',
+    py_modules=['pifacenetctl'],
+    license='GPL v3',
+    extras_require = {
+        'Control of netctl wireless profiles':["netctl>=0.2.0"],
+    }
+)
